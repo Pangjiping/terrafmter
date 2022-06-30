12ec46642032023187dba39f9c9ec3afcd0bf56a
pangjiping.pjp <pangjiping.pjp@alibaba-inc.com>
2022-06-30 19:52:59 +0800

    add ack datasource and resource list

--- terraform/r.go
+++ terraform/r.go
@@ -5,14 +5,14 @@ var ResourceMap map[string]string
 func init() {
 	ResourceMap = map[string]string{
 		// Container Service for Kubernetes (ACK)
-		"cs_kubernetes":             "",
-		"cs_managed_kubernetes":     "",
-		"cs_serverless_kubernetes":  "",
-		"cs_edge_kubernetes":        "",
-		"cs_autoscaling_config":     "",
-		"cs_kubernetes_addon":       "",
-		"cs_kubernetes_autoscaler":  "",
-		"cs_kubernetes_node_pool":   "",
-		"cs_kubernetes_permissions": "",
+		"cs_kubernetes":             "cs_kubernetes",
+		"cs_managed_kubernetes":     "cs_managed_kubernetes",
+		"cs_serverless_kubernetes":  "cs_serverless_kubernetes",
+		"cs_edge_kubernetes":        "cs_edge_kubernetes",
+		"cs_autoscaling_config":     "cs_autoscaling_config",
+		"cs_kubernetes_addon":       "cs_kubernetes_addon",
+		"cs_kubernetes_autoscaler":  "cs_kubernetes_autoscaler",
+		"cs_kubernetes_node_pool":   "cs_kubernetes_node_pool",
+		"cs_kubernetes_permissions": "cs_kubernetes_permissions",
 	}
 }
