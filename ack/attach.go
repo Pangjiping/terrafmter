c8b45947828243022a3db58fdcd017b7abea1603
pangjiping.pjp <pangjiping.pjp@alibaba-inc.com>
2022-07-05 14:19:05 +0800

    template enhancement

--- ack/attach.go
+++ ack/attach.go
@@ -4,8 +4,31 @@ package ack
 
 // attachMap key: resource or data
 //           value: name
-var attachMap map[string]string
+var (
+	attachResourceMap   map[string]string
+	attachDataSourceMap map[string]string
+)
 
 func init() {
-	attachMap = map[string]string{}
+	attachResourceMap = map[string]string{
+		"cs_kubernetes_node_pool":   "",
+		"cs_autoscaling_config":     "",
+		"cs_edge_kubernetes":        "",
+		"cs_kubernetes":             "",
+		"cs_kubernetes_autoscaler":  "",
+		"cs_kubernetes_permissions": "",
+		"cs_managed_kubernetes":     "vpc,vswitch,key_pair",
+		"cs_serverless_kubernetes":  "",
+	}
+
+	attachDataSourceMap = map[string]string{
+		"cs_kubernetes_node_pool":   "cs_kubernetes_clusters,cs_managed_kubernetes_clusters",
+		"cs_autoscaling_config":     "",
+		"cs_edge_kubernetes":        "",
+		"cs_kubernetes":             "",
+		"cs_kubernetes_autoscaler":  "",
+		"cs_kubernetes_permissions": "",
+		"cs_managed_kubernetes":     "zones,instance_types,cs_kubernetes_version",
+		"cs_serverless_kubernetes":  "",
+	}
 }
--- ack/future.md
+++ ack/future.md
@@ -7,3 +7,12 @@
 - ack required字段自动推荐
 - 针对ack产品的template增强，tags, runtime等带有elem的字段需要透出
 
+### ack深度优化
+**规则**
+- 命令行只输入ack resource时触发优化，例如`terrafmtter --resource cs_kubernetes`。
+- data source在前，resource在后面输出
+
+一些可能的优化项:
+- cs_kubernetes_node_pool、cs_kubernetes_autoscaler、cs_kubernetes_addon需要关联到cluster
+- cs_kubernetes，和cluster有关的一系列resource需要关联到依赖资源
+
